61ded41f91fcdb8c74fa39dfcaa1fe3fa91a59c1
tss <tss@debian-tss24>
2016-11-14 11:20:14 +0100

    Aggiunto input di una frase con output seconda versione aggiunta lez2

---
+++ src/lez2/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lez2;
+
+/**
+ * commenti vari
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        //singola riga
+        
+        /*
+        più
+        righe
+        */
+        System.out.println("start esempio commenti...");
+    }
+    
+}
