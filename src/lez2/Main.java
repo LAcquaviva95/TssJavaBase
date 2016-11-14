66832dd4835dedc2e096dd67f41eb39290673d75
tss <tss@debian-tss24>
2016-11-14 12:33:08 +0100

    Aggiunto input di una frase con output seconda versione aggiunta lez2

--- src/lez2/Main.java
+++ src/lez2/Main.java
@@ -7,23 +7,42 @@ package lez2;
 
 /**
  * commenti vari
+ *
  * @author tss
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    //variabile statica
+    private static int colore;
+    //variabile di istanza
+    private int carattere;
+
     public static void main(String[] args) {
-        // TODO code application logic here
-         
+        // args è una variabile locale del main che identifica un parametro
+
         //singola riga
-        
         /*
         più
         righe
-        */
+         */
         System.out.println("start esempio commenti...");
+        int totale = 10;
+        String frase;
+
+        if (totale > 0) {
+            int x = 5;
+            frase = "Il totale è positivo";
+        } else {
+            int x = 10;
+            frase = "il totale è negativo";
+
+        }
+
+        frase = (totale > 0) ? "il totale è positivo" : "il totale è negativo";
+
+        totale += 20;
+        System.out.println("totale: " + totale);
+        System.out.println(frase);
     }
-    
+
 }
